f2059badbf6963ed4da8c8aa5a6b8b641a616cca
ezamlee <a.m.essam1992@gmail.com>
2017-06-18 16:05:20 +0200

    fixed

--- models/logs.js
+++ models/logs.js
@@ -1,7 +1,7 @@
 var mongoose=require("mongoose");
 module.exports = mongoose.model('logs', new mongoose.Schema({
     time:{ type: Date, default: Date.now }
-    ,device:Number
+    ,device:String
     ,rssi:Number
     ,gateway:String
   })
